18b988d78e38bb616adeab0ced8083ed1e8c3379
LEARN Academy <ford.py@gmail.com>
2022-01-31 08:33:41 -0800

    Homework assignmnet

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,8 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+const { expect } = require("@jest/globals");
+const { array } = require("yargs");
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -19,11 +22,36 @@ const people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
+describe('makeSentences()', () => {
+  it('should make a sentence of a first and last name including an occupation', () => {
+    const result = [
+      'Ford Prefect is a hitchhiker',
+      'Zaphod Beeblebrox is president of the galaxy',
+      'Arthur Dent is a radio employee'
+    ];
+
+    expect(makeSentences(people)).toEqual(result);
+ });
+});
+
+// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 // b) Create the function that makes the test pass.
 
+// Loop through objects and transform them by name and occupation
+// Split the object name by first and last name (we have to call substring to index from 1 to the end)
+// Append the first and last name with occupation and make the sentence the correct
+
+const makeSentences = (people) => {
+  const newPeople = [];
+  for (let i = 0; i < people.length; i++) {
+    const [first, last] = people[i].name.split(' ').map(name => name[0].toUpperCase() + name.substring(1, name.length));
+    const sentence = first + ' '  + last + ' is ' + people[i].occupation;
+    newPeople.push(sentence);
+  }
+  return newPeople;
+};
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
@@ -34,9 +62,33 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe('getRemainders()', () => {
+  it('should make a new array of numbers with remainders', () => {
+    const result1 = [2, 0, -1, 0];
+    const result2 = [2, 1, -1];
+
+    expect(getRemainders(hodgepodge1)).toEqual(result1);
+    expect(getRemainders(hodgepodge2)).toEqual(result2)
+ });
+});
 
 // b) Create the function that makes the test pass.
 
+// Loop through the array and check if it's of type 'number'
+// We're only concerned with numbers
+// Then map each number by using the '%' operator 
+
+const getRemainders = (arr) => {
+  const numbers = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] === 'number') {
+      numbers.push(arr[i]);
+    }
+  }
+
+  return numbers.map(number => number % 3);  
+};
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -47,5 +99,20 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe('cubeSum()', () => {
+  it('should cube all the values in an array and sum them up', () => {
+    const [result1, result2] = [99, 1125];
+
+    expect(cubeSum(cubeAndSum1)).toEqual(result1);
+    expect(cubeSum(cubeAndSum2)).toEqual(result2);
+ });
+});
 
 // b) Create the function that makes the test pass.
+
+// Map all the values to be cubed (e.g., take every value and cube it)
+// Take this array, and sum them up with reduce
+
+const cubeSum = (arr) => {
+  return arr.map(number => Math.pow(number, 3)).reduce((a, b) => a + b);
+};
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,27 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) Blog post controller is inherenting from Application Controller which is a rails utility. 
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) Index is referencing the home page of the application and all the blog posts are being indexed here. 
     @posts = BlogPost.all
   end
 
   def show
-    # ---3)
+    # ---3) Show takes all the blog posts and finds it by a specific ID value. 
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) Blog post will allow you to create a new post model(e.g creating a new post in the database).
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) Create will take in parameters from the post request and create this new post. 
+    # If the post is valid it will re-direct to the new post path. 
+    # Else it will allow the user to create a new blog post. 
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -33,14 +35,14 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  # ---6)
+  # ---6) Edit is a method that will find a blog post by a specific ID value to edit and or modify. 
   def edit
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) It will find the post by ID and update it if it's valid, else re-direct them to edit the blog post. 
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +56,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) Redirect to blog post path. 
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) Private is a help method called blog_params. 
   private
   def blog_post_params
-    # ---10)
+    # ---10) This methodnrequires post request parameters of title and content. 
     params.require(:blog_post).permit(:title, :content)
   end
 end
